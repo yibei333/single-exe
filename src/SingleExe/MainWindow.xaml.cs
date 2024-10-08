a8695106634a13b3619308c870072e680a0e48c2
yibei333 <Developer333@hotmail.com>
2024-10-08 10:21:14 +0800

    change name

--- src/SingleExe/MainWindow.xaml.cs
+++ src/SingleExe/MainWindow.xaml.cs
@@ -14,49 +14,51 @@ public partial class MainWindow : Window
         StartAsync();
     }
 
-    async void StartAsync()
+    void StartAsync()
     {
-        await Task.Yield();
-        try
+        Task.Factory.StartNew(() =>
         {
-            var tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{App.Name}\\{App.Version}");
-            var integrityFile = Path.Combine(tempFolder, "integrity.txt");
-            var exePath = Path.Combine(tempFolder, App.EntryPoint);
-            if (!File.Exists(integrityFile) || !File.Exists(exePath))
+            try
             {
-                if (Directory.Exists(tempFolder)) Directory.Delete(tempFolder, true);
-                if (!Directory.Exists(tempFolder)) Directory.CreateDirectory(tempFolder);
-                var zipFile = Path.Combine(tempFolder, "Source.zip");
-                if (File.Exists(zipFile)) File.Delete(zipFile);
-
-                var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"SingleExe.Source.zip");
-                var zipStream = new FileStream(zipFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-                stream.CopyTo(zipStream);
-                zipStream.Flush();
-                zipStream.Dispose();
-                stream.Dispose();
-                ZipFile.ExtractToDirectory(zipFile, tempFolder);
-                File.Delete(zipFile);
-            }
-
-            if (!File.Exists(exePath)) throw new Exception($"file not found:{exePath}");
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo(exePath, string.Join(" ", Environment.GetCommandLineArgs()))
+                var tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{App.Name}\\{App.Version}");
+                var versionFile = Path.Combine(tempFolder, "version.txt");
+                var exePath = Path.Combine(tempFolder, App.EntryPoint);
+                if (!File.Exists(versionFile) || !File.Exists(exePath))
                 {
-                    WorkingDirectory = new FileInfo(exePath).DirectoryName,
-                    CreateNoWindow = true
-                }
-            };
-            process.Start();
+                    if (Directory.Exists(tempFolder)) Directory.Delete(tempFolder, true);
+                    if (!Directory.Exists(tempFolder)) Directory.CreateDirectory(tempFolder);
+                    var zipFile = Path.Combine(tempFolder, "Source.zip");
+                    if (File.Exists(zipFile)) File.Delete(zipFile);
 
-            if (!File.Exists(integrityFile)) File.Create(integrityFile).Close();
-            Application.Current.Shutdown();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message);
-            Application.Current.Shutdown(1);
-        }
+                    var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"SingleExe.Source.zip");
+                    var zipStream = new FileStream(zipFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    stream.CopyTo(zipStream);
+                    zipStream.Flush();
+                    zipStream.Dispose();
+                    stream.Dispose();
+                    ZipFile.ExtractToDirectory(zipFile, tempFolder);
+                    File.Delete(zipFile);
+                }
+
+                if (!File.Exists(exePath)) throw new Exception($"file not found:{exePath}");
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo(exePath, string.Join(" ", Environment.GetCommandLineArgs()))
+                    {
+                        WorkingDirectory = new FileInfo(exePath).DirectoryName,
+                        CreateNoWindow = true
+                    }
+                };
+                process.Start();
+
+                if (!File.Exists(versionFile)) File.WriteAllText(versionFile, App.Version);
+                Application.Current.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Current.Shutdown(1);
+            }
+        });
     }
 }
